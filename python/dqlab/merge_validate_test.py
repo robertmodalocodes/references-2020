ee503ee3d6f30485c06def325248b2901a8174c3
Robert Modalo <45115200+robertmodalocodes@users.noreply.github.com>
2024-02-18 07:20:11 +0800

    Add files via upload

---
+++ python/dqlab/merge_validate_test.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df1 = pd.DataFrame({
+   'key':['k1','k2','k3','k4','k5'],
+   'val1':[200, 500, 0, 500, 100],
+   'val2':[30, 50, 100, 20, 10],
+  
+})
+
+df2 = pd.DataFrame({
+   'key':['k1','k1','k5','k7','k10'],
+   'val3':[1,2,3,4,5],
+   'val4':[6,7,8,8,10]
+})
+
+merged = pd.merge(df1.reset_index(), df2.reset_index(), validate="1:1")
+print(merged)
